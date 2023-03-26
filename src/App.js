14bf4c9c23efb4644f0a1b22dbc806be8209f560
gaeeeguri <sjpark513@gm.gist.ac.kr>
2023-03-26 15:33:29 +0900

    feat(app): add more buttons

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import React, {useState} from 'react';
 const App = () => {
   const [count, setCount] = useState(0);
 
-  const onClick = () => setCount(prev => prev+5)
+  const onClick = (e) => setCount(prev => prev+parseInt(e.target.value))
 
   return (
     <>
     <div>
-      <button onClick={onClick}>
+      <button onClick={onClick} value={1}>
+        +1
+      </button><button onClick={onClick} value={5}>
         +5
+      </button><button onClick={onClick} value={-1}>
+        -1
+      </button><button onClick={onClick} value={-5}>
+        -5
       </button>
     </div>
     <div>
