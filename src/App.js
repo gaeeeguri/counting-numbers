b3f90690ae37bb486b2eda1c5880e41b5fdf0905
gaeeeguri <sjpark513@gm.gist.ac.kr>
2023-03-26 15:37:20 +0900

    feat(app): use map

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import React, {useState} from 'react';
 const App = () => {
   const [count, setCount] = useState(0);
 
-  const onClick = (e) => setCount(prev => prev+parseInt(e.target.value))
+  const onClick = (e) => setCount(prev => prev+parseInt(e.target.value));
+
+  const buttonValues = [1,5,-1,-5];
 
   return (
     <>
     <div>
-      <button onClick={onClick} value={1}>
-        +1
-      </button><button onClick={onClick} value={5}>
-        +5
-      </button><button onClick={onClick} value={-1}>
-        -1
-      </button><button onClick={onClick} value={-5}>
-        -5
-      </button>
+      {
+        buttonValues.map((buttonValue) =>
+          <button
+            key={buttonValue}
+            value={buttonValue}
+            onClick={onClick}
+          >
+            {buttonValue > 0 && '+'}{buttonValue}
+          </button>)
+      }
     </div>
     <div>
       count: {count}
