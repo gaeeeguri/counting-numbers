a2337fc82797657099534006852ee9333847419c
gaeeeguri <sjpark513@gm.gist.ac.kr>
2023-03-26 15:30:22 +0900

    feat(app): add button and count

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
-const App = () =><div>hello</div>
+import React, {useState} from 'react';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    <div>
+      <button>
+        +5
+      </button>
+    </div>
+    <div>
+      count: {count}
+    </div>
+  </>
+  )
+}
 
 export default App;
